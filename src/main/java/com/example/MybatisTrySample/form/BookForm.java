c4aa7aa6e61320fccf5f727eb38ed5272201fbf8
koki <kokides0428@gmail.com>
2022-08-24 23:49:54 +0900

    gitブランチ枝分かれ用

--- src/main/java/com/example/MybatisTrySample/form/BookForm.java
+++ src/main/java/com/example/MybatisTrySample/form/BookForm.java
@@ -15,6 +15,8 @@ public class BookForm {
     private String bookName;
     private Integer volumeNum;
     private String authorName;
+
+//    このアノテーションをつけないと日付データ受け取れない 2022-08-22形式でデータ受け取る
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate publishedDate;
 }
