c042323b897d07ef897396c7217af1545524659a
koki <kokides0428@gmail.com>
2022-10-13 22:26:29 +0900

    findById、findAll　テストコード作成

--- src/test/java/com/example/SpringTrySample/dao/BookDaoTest.java
+++ src/test/java/com/example/SpringTrySample/dao/BookDaoTest.java
@@ -6,20 +6,17 @@ import com.ninja_squad.dbsetup.Operations;
 import com.ninja_squad.dbsetup.destination.DataSourceDestination;
 import com.ninja_squad.dbsetup.destination.Destination;
 import com.ninja_squad.dbsetup.operation.Operation;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
-
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static com.ninja_squad.dbsetup.Operations.sequenceOf;
@@ -27,11 +24,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestExecutionListeners({
-        DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
-        TransactionalTestExecutionListener.class}
-)
+@Rollback
+@Transactional
 class BookDaoTest {
 
     @Autowired
@@ -40,57 +34,137 @@ class BookDaoTest {
     @Autowired
     private DataSource dataSource;
 
-    Destination dest = new DataSourceDestination(dataSource);
+    Destination dest;
 
     private final Operation RESET_DATA = Operations.deleteAllFrom("libraryschema.booktable");
     private final Operation INSERT_BOOKTABLE = Operations.insertInto("libraryschema.booktable")
             .columns("id","book_name","volume_num", "author_name", "published_date")
-            .values(1, "HUNTER X HUNTER",36,"冨樫義博","'2018-10-04")
+            .values(1, "HUNTER X HUNTER",36,"冨樫義博","2018-10-04")
             .values(2, "ベルセルク",40,"三浦健太郎","2018-09-28")
             .build();
 
-//    private void dbSetup(Operation operation) {
-//        Destination destination = new DataSourceDestination(dataSource);
-//        DbSetup dbSetup = new DbSetup(destination, operation);
-//        dbSetup.launch();
-//    }
+    public void dbSetup(Operation operation) {
+        dest = new DataSourceDestination(dataSource);
+        DbSetup dbSetup = new DbSetup(dest, operation);
+        dbSetup.launch();
+    }
 
     @BeforeEach
-    void setUp(){
-        Operation ops = sequenceOf(RESET_DATA, INSERT_BOOKTABLE);
-        DbSetup dbSetup = new DbSetup(dest, ops);
-//        dbSetup(Operations.sequenceOf(RESET_DATA));
+    public void setUp(){
+        dbSetup(sequenceOf(RESET_DATA, INSERT_BOOKTABLE));
     }
 
-//    @AfterEach
-//    void tearDown(){
-//        dbSetup(sequenceOf(RESET_DATA));
-//    }
-
-//    @Test
-//    void findById() {
-//        dbSetup(Operations.sequenceOf(INSERT_BOOKTABLE));
-//        Book checkBook = new Book();
-//        checkBook.setId(1);
-//        Optional<Book> actualBook = bookDao.findById(checkBook);
-//        assertThat(actualBook)
-//                .isNotEmpty()
-//                .isNotNull();
-//    }
-
-    @Test
-    void findAll() {
+    @AfterEach
+    public void tearDown(){
+        dbSetup(sequenceOf(RESET_DATA));
     }
 
-    @Test
-    void insertBook() {
+    @Nested
+    public class findById{
+        @Test
+        @DisplayName("findById_書籍情報の取得成功")
+        public void findByIdSuccess() {
+            Book checkBook = new Book();
+            checkBook.setId(1);
+            Optional<Book> actualBookOpt = bookDao.findById(checkBook);
+            Book actualBook = actualBookOpt.get();
+
+            assertThat(actualBook).extracting("id").isEqualTo(1);
+            assertThat(actualBook).extracting("bookName").isEqualTo("HUNTER X HUNTER");
+            assertThat(actualBook).extracting("volumeNum").isEqualTo(36);
+            assertThat(actualBook).extracting("authorName").isEqualTo("冨樫義博");
+            assertThat(actualBook).extracting("publishedDate").isEqualTo(LocalDate.parse("2018-10-04"));
+        }
+
+        @Test
+        @DisplayName("findById_書籍情報の取得失敗")
+        public void findByIdNotFailure() {
+            Book checkBook = new Book();
+            checkBook.setId(5);
+            Optional<Book> actualBookOpt = bookDao.findById(checkBook);
+
+            assertThat(actualBookOpt).isEmpty();
+        }
+
     }
 
-    @Test
-    void updateBook() {
+    @Nested
+    public class findAll{
+        @Test
+        @DisplayName("findAll_書籍情報一覧の取得成功")
+        public void findAllSuccess() {
+            List<Book> actualBookList = bookDao.findAll();
+            assertThat(actualBookList).isNotNull();
+            assertThat(actualBookList).extracting("id").allMatch(i -> Arrays.asList(1,2).contains(i));
+            assertThat(actualBookList).extracting("bookName").allMatch(i -> Arrays.asList("HUNTER X HUNTER","ベルセルク").contains(i));
+            assertThat(actualBookList).extracting("volumeNum").allMatch(i -> Arrays.asList(36,40).contains(i));
+            assertThat(actualBookList).extracting("authorName").allMatch(i -> Arrays.asList("冨樫義博","三浦健太郎").contains(i));
+            assertThat(actualBookList).extracting("publishedDate").allMatch(i -> Arrays.asList(LocalDate.parse("2018-10-04"),LocalDate.parse("2018-09-28")).contains(i));
+        }
     }
 
-    @Test
-    void deleteBook() {
+    @Nested
+    public class insertBook{
+        @Test
+        @DisplayName("insertBook_書籍情報の追加成功")
+        public void insertBookSuccess() {
+            Book book = new Book(3, "HUNTER X HUNTER",36,"冨樫義博",LocalDate.parse("2018-10-04"));
+            bookDao.insertBook(book);
+
+            Book checkBook = new Book();
+            checkBook.setId(3);
+            Optional<Book> actualBookOpt = bookDao.findById(checkBook);
+            Book actualBook = actualBookOpt.get();
+
+            assertThat(actualBook).extracting("id").isEqualTo(3);
+            assertThat(actualBook).extracting("bookName").isEqualTo("HUNTER X HUNTER");
+            assertThat(actualBook).extracting("volumeNum").isEqualTo(36);
+            assertThat(actualBook).extracting("authorName").isEqualTo("冨樫義博");
+            assertThat(actualBook).extracting("publishedDate").isEqualTo(LocalDate.parse("2018-10-04"));
+
+        }
+
+        @Test
+        @DisplayName("insertBook_書籍情報の追加失敗")
+        public void insertBookFailure(){
+
+
+        }
+
     }
+
+    @Nested
+    public class updateBook{
+        @Test
+        @DisplayName("updateBook_書籍情報の更新成功")
+        public void updateBookSuccess() {
+            Book book = new Book(1, "UpdateTest",36,"冨樫義博",LocalDate.parse("2018-10-04"));
+            Boolean check =  bookDao.updateBook(book);
+            assertThat(check).isTrue();
+        }
+
+        @Test
+        @DisplayName("updateBook_書籍情報の更新失敗")
+        public void updateBookFailure() {
+            Book book = new Book(4, "UpdateTest",36,"冨樫義博",LocalDate.parse("2018-10-04"));
+            Boolean check =  bookDao.updateBook(book);
+            assertThat(check).isFalse();
+        }
+    }
+
+    @Nested
+    public class deleteBook{
+        @Test
+        @DisplayName("deleteBook_書籍情報の削除成功")
+        public void deleteBookSuccess() {
+
+        }
+
+        @Test
+        @DisplayName("deleteBook_書籍情報の削除失敗")
+        public void deleteBookFailure(){
+
+        }
+    }
+
 }
