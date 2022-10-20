ab24fadf7b73ad63b958daeeef32fa81cdf57668
koki <kokides0428@gmail.com>
2022-10-20 13:15:46 +0900

    BookDaoTest一旦全てのメソッドに正常系と異常系のテストコードを作成。insert、update、deleteの正常系テストに課題あり。

--- src/test/java/com/example/SpringTrySample/dao/BookDaoTest.java
+++ src/test/java/com/example/SpringTrySample/dao/BookDaoTest.java
@@ -6,9 +6,12 @@ import com.ninja_squad.dbsetup.Operations;
 import com.ninja_squad.dbsetup.destination.DataSourceDestination;
 import com.ninja_squad.dbsetup.destination.Destination;
 import com.ninja_squad.dbsetup.operation.Operation;
+import org.assertj.db.type.Changes;
+import org.assertj.db.type.Table;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,12 +24,13 @@ import java.util.Optional;
 
 import static com.ninja_squad.dbsetup.Operations.sequenceOf;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest
-@ActiveProfiles("test")
-@Rollback
-@Transactional
-class BookDaoTest {
+@SpringBootTest  // @SpringBootConfigurationがついたクラスが自動検出される
+@ActiveProfiles("test")  // application-test.ymlの内容が読み込まれる
+@Rollback  // @Transactionalがあればいらない。。？
+@Transactional //　トランザクション管理される
+public class BookDaoTest {
 
     @Autowired
     private BookDao bookDao;
@@ -81,9 +85,10 @@ class BookDaoTest {
         public void findByIdNotFailure() {
             Book checkBook = new Book();
             checkBook.setId(5);
-            Optional<Book> actualBookOpt = bookDao.findById(checkBook);
 
-            assertThat(actualBookOpt).isEmpty();
+            assertThatThrownBy(() -> {
+                Optional<Book> actualBookOpt = bookDao.findById(checkBook);
+            }).isInstanceOf(NullPointerException.class);
         }
 
     }
@@ -101,6 +106,7 @@ class BookDaoTest {
             assertThat(actualBookList).extracting("authorName").allMatch(i -> Arrays.asList("冨樫義博","三浦健太郎").contains(i));
             assertThat(actualBookList).extracting("publishedDate").allMatch(i -> Arrays.asList(LocalDate.parse("2018-10-04"),LocalDate.parse("2018-09-28")).contains(i));
         }
+
     }
 
     @Nested
@@ -108,34 +114,37 @@ class BookDaoTest {
         @Test
         @DisplayName("insertBook_書籍情報の追加成功")
         public void insertBookSuccess() {
+            final Table table = new Table(dataSource, "libraryschema.booktable");
+            Changes changes = new Changes(table);
+
+            changes.setStartPointNow();
+
             Book setBook = new Book();
-            setBook.setId(3);
             setBook.setBookName("HUNTER X HUNTER");
             setBook.setVolumeNum(36);
             setBook.setAuthorName("冨樫義博");
             setBook.setPublishedDate(LocalDate.parse("2018-10-04"));
             bookDao.insertBook(setBook);
 
-
-            List<Book> actualBookList = bookDao.findAll();
-
-            Book checkBook = new Book();
-            checkBook.setId(3);
-            Optional<Book> actualBookOpt = bookDao.findById(checkBook);
-            Book actualBook = actualBookOpt.get();
-
-            assertThat(actualBook).extracting("id").isEqualTo(3);
-            assertThat(actualBook).extracting("bookName").isEqualTo("HUNTER X HUNTER");
-            assertThat(actualBook).extracting("volumeNum").isEqualTo(36);
-            assertThat(actualBook).extracting("authorName").isEqualTo("冨樫義博");
-            assertThat(actualBook).extracting("publishedDate").isEqualTo(LocalDate.parse("2018-10-04"));
+            changes.setEndPointNow();
+            org.assertj.db.api.Assertions.assertThat(changes)
+                    .hasNumberOfChanges(1)   //1レコード変更された確認　ここでどうしても失敗する。なぜか変更箇所が0
+                    .ofModificationOnTable("libraryschema.booktable");
 
         }
 
         @Test
         @DisplayName("insertBook_書籍情報の追加失敗")
         public void insertBookFailure(){
+            Book setBook = new Book();
+            setBook.setBookName("HUNTER X HUNTER");
+            setBook.setVolumeNum(36);
+            setBook.setAuthorName(null);
+            setBook.setPublishedDate(LocalDate.parse("2018-10-04"));
 
+            assertThatThrownBy(() -> {
+                bookDao.insertBook(setBook);
+            }).isInstanceOf(DataIntegrityViolationException.class);
 
         }
 
@@ -143,12 +152,23 @@ class BookDaoTest {
 
     @Nested
     public class updateBook{
+        /**
+         * なぜかテスト実行すると、ずっとぐるぐる周り続けてテストが終わらない。。
+         */
         @Test
         @DisplayName("updateBook_書籍情報の更新成功")
         public void updateBookSuccess() {
+            Changes changes = new Changes(dataSource);
+
+            changes.setStartPointNow();
             Book book = new Book(1, "UpdateTest",36,"冨樫義博",LocalDate.parse("2018-10-04"));
             Boolean check =  bookDao.updateBook(book);
-            assertThat(check).isTrue();
+
+            changes.setEndPointNow();
+            org.assertj.db.api.Assertions.assertThat(changes)
+                    .hasNumberOfChanges(1);
+
+
         }
 
         @Test
@@ -162,16 +182,32 @@ class BookDaoTest {
 
     @Nested
     public class deleteBook{
+
+        /**
+         * なぜかテスト実行すると、ずっとぐるぐる周り続けてテストが終わらない。。
+         */
         @Test
         @DisplayName("deleteBook_書籍情報の削除成功")
         public void deleteBookSuccess() {
+            Changes changes = new Changes(dataSource);
+
+            changes.setStartPointNow();
+            Book book = new Book();
+            book.setId(1);
+            Boolean check =  bookDao.deleteBook(book);
+            changes.setEndPointNow();
+            org.assertj.db.api.Assertions.assertThat(changes)
+                    .hasNumberOfChanges(1);
 
         }
 
         @Test
         @DisplayName("deleteBook_書籍情報の削除失敗")
         public void deleteBookFailure(){
-
+            Book book = new Book();
+            book.setId(3);
+            Boolean check =  bookDao.deleteBook(book);
+            assertThat(check).isFalse();
         }
     }
 
